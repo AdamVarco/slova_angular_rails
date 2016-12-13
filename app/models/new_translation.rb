0c73cbdec6cb331399db47716aa41aa7e67b1aed
Grant <gcg4wg@virginia.edu>
2016-12-13 14:57:24 -0500

    reorganize file structure

--- app/models/new_translation.rb
+++
@@ -1,3 +0,0 @@
-class NewTranslation < ApplicationRecord
-    
-end
--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-
-  def self.from_token_payload(payload)
-    find_or_create_by(
-      email: payload['email'],
-      auth0_id_string: payload['sub']
-    )
-  end
-end
