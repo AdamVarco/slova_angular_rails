89022be2b2b23bb14ce01e5c863d80bcddb99432
Grant <gcg4wg@virginia.edu>
2016-12-19 09:23:22 -0500

    staging for deploying

--- rails/spec/models/user_spec.rb
+++ rails/spec/models/user_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:email).of_type(:string) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
-    it { is_expected.to have_db_column(:auth0_id_string).of_type(:string) }
   end
 
   describe "validations" do
